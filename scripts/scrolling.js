6e9826f5dbfd8a5b3d2d68d435d3cc71ec2161eb
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-03 15:13:31 +0200

    added a logo for the nav bar

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -13,12 +13,16 @@ function scrollFunction() {
     document.getElementById("logo").style.width = "20%";
     document.getElementById("logo").style.padding = "auto";
     document.getElementById("logo").style.margin = "0";
+    document.getElementById("logo").getElementsByTagName('img')[0].src = "assets/landing-page-imgs/logo-footer.png";
 
   } else {
     document.getElementById("navbar").style.backgroundColor = "transparent";
     document.getElementById("navbar").style.opacity = "1";
     document.getElementById("logo").style.margin = "10px";
     document.getElementById("logo").style.width = "auto";
+    
+    document.getElementById("logo").getElementsByTagName('img')[0].src = "./assets/landing-page-imgs/logo-light.png";
+    
 
   }
 }
