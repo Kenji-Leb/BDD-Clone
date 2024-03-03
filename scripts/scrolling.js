4dbc5a36deb8b76bae618f11a8f1369fd5c71154
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-03 12:01:56 +0200

    fixing logo

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -10,12 +10,16 @@ function scrollFunction() {
     document.getElementById("navbar").style.backgroundColor = "black";
     document.getElementById("navbar").style.opacity = ".9";
     document.getElementById("navbar").style.zIndex = "1000";
-    document.getElementById("logo").style.width = "25%";
+    document.getElementById("navbar").style.minHeight = "15%";
+    document.getElementById("logo").style.width = "20%";
+    document.getElementById("logo").style.padding = "auto";
+    document.getElementById("logo").style.margin = "0";
 
   } else {
     // scrolling up
     document.getElementById("navbar").style.backgroundColor = "transparent";
     document.getElementById("navbar").style.opacity = "1";
+    document.getElementById("logo").style.margin = "10px";
     document.getElementById("logo").style.width = "auto";
 
   }
